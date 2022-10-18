1eb94370dc11764345842f2d0af14632b8d3a6ed
Tri-Nguyen-C0822g1 <tringuyen10396@gmail.com>
2022-10-18 14:10:43 +0700

    add

---
+++ src/AbsoluteNumberCalculator.java
@@ -0,0 +1,10 @@
+
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number){
+        return Math.abs(number);
+//        throw new UnsupportedOperationException();
+    }
+
+    }
+
---
+++ src/AbsoluteNumberCalculatorTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbsoluteNumberCalculatorTest {
+
+    @Test
+    void findAbsolute0() {
+        int number = -1;
+        int expected = 1;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+}
